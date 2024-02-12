67df394d9f46ff13c38b981e58ae60f1caca132b
Rain Hu <intervalrain@gmail.com>
2024-02-12 16:55:26 +0800

    init

---
+++ Server/Hubs/BlazerChatSampleHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Server.Hubs;
+
+public class BlazerChatSampleHub : Hub
+{
+	public const string hubUrl = "/chat";
+
+	public async Task BroadCast(string userName, string message)
+	{
+		await Clients.All.SendAsync("Broadcast", userName, message);
+	}
+
+	public override Task OnConnectedAsync()
+	{
+		Console.WriteLine($"{Context.ConnectionId} connected");
+		return base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? e)
+    {
+		Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+        await base.OnDisconnectedAsync(e);
+    }
+}
+
